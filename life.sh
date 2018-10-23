d58f6640a5c8c7fee8e50cf3848547de6c69693b
camille <camille@lzr.pw>
2018-10-23 21:11:50 +0200

    minor changes

--- life.sh
+++ life.sh
@@ -11,8 +11,7 @@ for ((; i++ < INIT;)) do
         x = RANDOM % X,
         y = RANDOM % Y,
 
-        gol[$x,$y] = 1,
-        tmp[$x,$y] = 1
+        gol[$x $y] = 1
     ))
 done
 
@@ -20,14 +19,14 @@ check ()
 {
     c=
 
-    ((gol[$i,$j] == 1 && c--))
+    ((gol[$i $j] == 1 && c--))
 
     for ((x = i - 1; x <= i + 1; x++)) do
         for ((y = j - 1; y <= j + 1; y++)) do
             ((
                 x >= 0 && x < X &&
                 y >= 0 && y < Y &&
-                gol[$x,$y] == 1 && c++
+                gol[$x $y] == 1 && c++
             ))
         done
     done
@@ -40,8 +39,8 @@ update ()
             check
 
             ((
-                tmp[$i,$j] = c == 2 ?
-                tmp[$i,$j] : c == 3 ? 1 : 0
+                tmp[$i $j] = c == 2 ?
+                tmp[$i $j] : c == 3 ? 1 : 0
             ))
         done
     done
@@ -57,7 +56,7 @@ display ()
 
     for ((i = 0; i < X; i++)) do
         for ((j = 0; j < Y; j++)) do
-            ((gol[$i,$j] == 1)) && p+=o || p+=' '
+            ((gol[$i $j] == 1)) && p+=o || p+=' '
         done
         p+='\n'
     done

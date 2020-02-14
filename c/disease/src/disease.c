d891cc5b852d94991af1f4758cc644f58d84b052
turquoise-hexagon <camille@lzr.pw>
2020-02-14 13:10:55 +0100

    speed up code

--- c/disease/src/disease.c
+++ c/disease/src/disease.c
@@ -1,6 +1,5 @@
 #include <err.h>
 #include <errno.h>
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -24,96 +23,85 @@ main(int argc, char** argv)
     if (argc != 3)
         errx(1, "usage : %s [height] [width]", argv[0]);
 
-    unsigned i, j, k;
+    unsigned i, j;
 
     errno = 0;
     char* ptr;
-    long arg[2];
+    long a[2];
 
-    for (i = 0; i < sizeof(arg) / sizeof(long); i++) {
-        arg[i] = strtol(argv[i + 1], &ptr, 10);
+    for (i = 0; i < sizeof(a) / sizeof(long); i++) {
+        a[i] = strtol(argv[i + 1], &ptr, 10);
 
-        if (errno != 0 || *ptr != 0 || arg[i] < 0)
-            errx(1, "invalid parameter");
+        if (errno != 0 || *ptr != 0 || a[i] < 0)
+            errx(1, "error : '%s' invalid parameter", argv[i + 1]);
     }
 
     /* init array */
-    unsigned short** uni = malloc(arg[0] * sizeof(unsigned short*));
+    unsigned short** uni = malloc(a[0] * sizeof(unsigned short*));
 
     if (uni == NULL)
-        errx(1, "failed to allocate memory");
+        errx(1, "error : failed to allocate memory");
 
-    for (i = 0; i < arg[0]; i++) {
-        uni[i] = calloc(arg[1], sizeof(unsigned short));
+    for (i = 0; i < a[0]; i++) {
+        uni[i] = calloc(a[1], sizeof(unsigned short));
 
         if (uni[i] == NULL)
-            errx(1, "failed to allocate memory");
+            errx(1, "error : failed to allocate memory");
     }
 
-    uni[arg[0] / 2][arg[1] / 2] = 1;
-
-    long x, y;
-    unsigned tmp;
-    unsigned short bin = 0;
-    srand(time(NULL));
+    uni[a[0] / 2][a[1] / 2] = 1;
 
     /* run disease */
+    short k;
+    unsigned x, y, tmp;
+    srand(time(NULL));
+
     for (unsigned n = 0; n < N; n++) {
-        printf("P4\n%ld %ld\n", arg[1], arg[0]);
+        printf("P1\n%ld %ld\n", a[1], a[0]);
 
-        for (i = 0; i < arg[0]; i++)
-            for (j = 0; j < arg[1] + arg[1] % 8; j++) {
-                bin <<= true;
+        for (i = 0; i < a[0]; i++)
+            for (j = 0; j < a[1]; j++) {
+                tmp = rand();
 
-                if (j < arg[1]) {
-                    tmp = rand();
+                switch (uni[i][j]) {
+                    case 0 :
+                        putchar('0');
 
-                    switch (uni[i][j]) {
-                        case 0 :
-                            bin |= false;
+                        if (tmp % 10 == 0)
+                            for (k = 0; k < 4; k++) {
+                                WRAP(x, i, DIRS[k][0], a[0]);
+                                WRAP(y, j, DIRS[k][1], a[1]);
 
-                            if (tmp % 10 == 0)
-                                for (k = 0; k < 4; k++) {
-                                    WRAP(x, i, DIRS[k][0], arg[0]);
-                                    WRAP(y, j, DIRS[k][1], arg[1]);
+                                if (uni[x][y] != 1)
+                                    uni[x][y] = 0;
+                            }
 
-                                    if (uni[x][y] != 1)
-                                        uni[x][y] = 0;
-                                }
+                        break;
+                    case 1 :
+                        putchar('0');
 
-                            break;
-                        case 1 :
-                            bin |= false;
+                        if (tmp % 2  == 0)
+                            for (k = 0; k < 4; k++) {
+                                WRAP(x, i, DIRS[k][0], a[0]);
+                                WRAP(y, j, DIRS[k][1], a[1]);
 
-                            if (tmp % 2  == 0)
-                                for (k = 0; k < 4; k++) {
-                                    WRAP(x, i, DIRS[k][0], arg[0]);
-                                    WRAP(y, j, DIRS[k][1], arg[1]);
+                                if (uni[x][y] == 0)
+                                    uni[x][y] = 1;
+                            }
+                        else if (tmp / 2  %  10 == 0) uni[i][j] = 2;
+                        else if (tmp / 20 % 500 == 0) uni[i][j] = 0;
 
-                                    if (uni[x][y] == 0)
-                                        uni[x][y] = 1;
-                                }
-                            else if (tmp / 2  %  10 == 0) uni[i][j] = 2;
-                            else if (tmp / 20 % 500 == 0) uni[i][j] = 0;
-
-                            break;
-                        default :
-                            bin |= true;
-                    }
+                        break;
+                    default :
+                        putchar('1');
                 }
-                else
-                    bin |= true;
-
-                if (j % 8 == 7)
-                    putchar(bin);
             }
     }
 
     /* cleanup */
-    for (i = 0; i < arg[0]; i++)
+    for (i = 0; i < a[0]; i++)
         free(uni[i]);
 
     free(uni);
-
     return 0;
 }
